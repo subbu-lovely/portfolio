d3ee7608c0da0b9dc2e2e2b6b9c5df8209e850b7
unknown <subbukondrupathi@gmail.com>
2025-07-23 11:11:41 +0530

    Manual deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',  // <-- use all lowercase if your repo name is lowercase
   plugins: [react()],
-})
+});
